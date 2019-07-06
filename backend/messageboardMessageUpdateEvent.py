1fe23943379a9f7078cb74e69d8bb42cd50a2b7e
Tangb <tanguy.bonneau@gmail.com>
2019-07-06 10:40:44 +0200

    Update event after core update

--- backend/messageboardMessageUpdateEvent.py
+++ backend/messageboardMessageUpdateEvent.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from raspiot.events.event import Event
+from raspiot.libs.internals.event import Event
 
 class MessageboardMessageUpdateEvent(Event):
     """
@@ -10,6 +10,7 @@ class MessageboardMessageUpdateEvent(Event):
 
     EVENT_NAME = u'messageboard.message.update'
     EVENT_SYSTEM = False
+    EVENT_PARAMS = [u'nomessage', u'off', u'message']
 
     def __init__(self, bus, formatters_broker, events_broker):
         """ 
@@ -22,14 +23,3 @@ class MessageboardMessageUpdateEvent(Event):
         """
         Event.__init__(self, bus, formatters_broker, events_broker)
 
-    def _check_params(self, params):
-        """
-        Check event parameters
-
-        Args:
-            params (dict): event parameters
-
-        Return:
-            bool: True if params are valid, False otherwise
-        """
-        return all(key in [u'nomessage', u'off', u'message'] for key in params.keys())
